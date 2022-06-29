4250afb36f800bcb6495c615c2c6a474a215f5ba
Eric Di Re <E72219@ad.carvana.com>
2022-06-29 10:44:47 -0700

    Adjusted setup.py to include downloading SharedVBA.bas file

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(
     url='https://github.com/edire/my_excel.git',
     python_requires='>=3.6',
     packages=['my_excel'],
+    package_data={'my_excel': ['MiscFiles/SharedVBA.bas']},
     install_requires=['pywin32']
 )
